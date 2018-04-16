8271c0af527799b8683d29f8d3448eff56fa1603
Kay Ogunbowale <o.ogunbowal@gmail.com>
2018-04-16 13:13:13 +0100

    Created  the AddReminder

---
+++ app/src/main/java/com/android/prod/mediapp/view/activity/AddReminder.java
@@ -0,0 +1,15 @@
+package com.android.prod.mediapp.view.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.android.prod.mediapp.R;
+
+public class AddReminder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_reminder);
+    }
+}
--- app/src/main/java/com/android/prod/mediapp/view/activity/MainActivity.java
+++ app/src/main/java/com/android/prod/mediapp/view/activity/MainActivity.java
@@ -3,12 +3,17 @@ package com.android.prod.mediapp.view.activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 
 import com.android.prod.mediapp.R;
+import com.github.jhonnyx2012.horizontalpicker.DatePickerListener;
+import com.github.jhonnyx2012.horizontalpicker.HorizontalPicker;
 
-public class MainActivity extends AppCompatActivity {
+import org.joda.time.DateTime;
+
+public class MainActivity extends AppCompatActivity implements DatePickerListener {
 
     FloatingActionButton fab;
     FloatingActionButton fab_add_medication;
@@ -23,41 +28,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        INIT();
+        HorizontalPicker picker = findViewById(R.id.datePicker);
+
+        picker
+                .setListener(this)
+                .init();
+
+
     }
 
 
     private void INIT(){
 
-        fabBGLayout=findViewById(R.id.fabBGLayout);
-        fab = findViewById(R.id.fab);
-        fab_add_medication = findViewById(R.id.fab_Add_Reminder);
-        fab_edit_medication = findViewById(R.id.fab_Edit_Reminder);
-
-        LL_add_medication = findViewById(R.id.fabLayout1);
-        LL_edit_medication = findViewById(R.id.fabLayout2);
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (!isFABOpen){
-                    showFabMenu();
-                }
-            }
-        });
-
     }
 
-    public void showFabMenu(){
-        isFABOpen = true;
-        fabBGLayout.setVisibility(View.VISIBLE);
 
-        LL_add_medication.setVisibility(View.VISIBLE);
-        LL_edit_medication.setVisibility(View.VISIBLE);
-
-        fab.animate().rotationBy(180);
-        fab_add_medication.animate().translationY(-55);
-        fab_edit_medication.animate().translationY(-100);
-
+    @Override
+    public void onDateSelected(DateTime dateSelected) {
+        Log.i("Horizontal picker", "selected date is " + dateSelected.toString());
     }
 }
